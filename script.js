643b52f60eb955134839fc5b9c2994964c3a86f1
Rutuja-Bhapkar <rutujabhapkar855@gmail.com>
2025-09-08 13:17:59 +0530

    files of html css js is added

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("alertBtn");
+
+  button.addEventListener("click", function () {
+    alert("Welcome! You clicked the professional button.");
+  });
+});
